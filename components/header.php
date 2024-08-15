dcb331a06424a4160662d09c0a61450808eec6d2
Anthony Kildery <90473131+anthonyKld@users.noreply.github.com>
2024-08-15 13:42:49 -0400

    barra

--- components/header.php
+++ components/header.php
@@ -83,10 +83,10 @@ if (!isset($_SESSION['usuario'])) {
                             <i class="material-icons">keyboard_arrow_down</i>
                         </a>
                         <div class="mdl-navigation">
-                            <a class="mdl-navigation__link" href='/src/pages/vendedores.php'>
+                            <a class="mdl-navigation__link" href='/../src/pages/vendedores.php'>
                                 Vendedores //ok
                             </a>
-                            <a class="mdl-navigation__link" href='/src/pages/historico-pedidos.php'>
+                            <a class="mdl-navigation__link" href='/../src/pages/historico-pedidos.php'>
                                 Histórico //ok
                             </a>
                             <a class="mdl-navigation__link" href='https://floralwhite-kudu-117908.hostingersite.com/src/pages/historico-pedidos.php'>
