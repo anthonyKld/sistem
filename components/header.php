67194ff7ac7be2b1eaf586a895bdfe8f6f21ac24
Anthony Kildery <90473131+anthonyKld@users.noreply.github.com>
2024-08-15 13:31:51 -0400

    teste

--- components/header.php
+++ components/header.php
@@ -84,16 +84,23 @@ if (!isset($_SESSION['usuario'])) {
                         </a>
                         <div class="mdl-navigation">
                             <a class="mdl-navigation__link" href="pages/vendedores.php">
-                                Vendedores
+                                Vendedores //ok
                             </a>
                             <a class="mdl-navigation__link" href="src/pages/dados-empresa.php">
-                                Dados da empresa
+                                Dados da empresa //https://floralwhite-kudu-117908.hostingersite.com/src/src/pages/dados-empresa.php
                             </a>
                             <a class="mdl-navigation__link" href="../src/pages/historico-pedidos.php">
-                                Histórico
+                                Histórico //ok
                             </a>
                             <a class="mdl-navigation__link" href="https://floralwhite-kudu-117908.hostingersite.com/src/pages/historico-pedidos.php">
-                                Histórico
+                                Histórico //ok
+                            </a>
+
+                            <a class="mdl-navigation__link" href="pages/vendedores.php">
+                                1 //ok
+                            </a>
+                            <a class="mdl-navigation__link" href="pages/dados-empresa.php">
+                                2 //https://floralwhite-kudu-117908.hostingersite.com/src/src/pages/dados-empresa.php
                             </a>
                         </div>
                     </div>
